1e703acb0343d8bdbc56b4328bff6e4ffa8c0a31
Claupio <claudio.riccio05@gmail.com>
2026-07-02 13:44:41 +0200

    aggiunta funzione di cancellazione ordine a consumatore service

--- src/app/services/consumatore-service.ts
+++ src/app/services/consumatore-service.ts
@@ -33,4 +33,30 @@ export class ConsumatoreService {
   modificaPasswordDaMail(token: string, new_password: string) : Observable<any> {
     return this.httpClient.post(this.baseURL + "/recover_password", {token, new_password})
   }
+
+  getOrdiniConsumatore(filtri: any) {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.getToken()}`
+    });
+
+    console.log("Token in getOrdiniConsumatore:", this.getToken());
+
+    let params = new HttpParams();
+
+    for(const [key, value] of Object.entries(filtri)) {
+      params = params.set(key, JSON.stringify(value))
+    }
+
+    return this.httpClient.get<any>(`${this.baseURL}/consumatore/ordini`, { headers, params});
+  }
+
+  cancellaOrdine(ordineId: number): Observable<any> {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.getToken()}`
+    });
+
+    return this.httpClient.put<any>(`${this.baseURL}/consumatore/ordini/cancel`, {ordine_id: ordineId}, { headers });
+  }
+
+  
 }
