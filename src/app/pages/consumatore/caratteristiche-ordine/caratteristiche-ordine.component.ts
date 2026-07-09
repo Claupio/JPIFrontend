2e7b2732819ad69944c10f694094240688dc43b3
Claupio <claudio.riccio05@gmail.com>
2026-07-09 18:28:48 +0200

    fixata mappa ordini

--- src/app/pages/consumatore/caratteristiche-ordine/caratteristiche-ordine.component.ts
+++ src/app/pages/consumatore/caratteristiche-ordine/caratteristiche-ordine.component.ts
@@ -163,31 +163,30 @@ throw new Error('Method not implemented.');
   cambiaVista(vista: 'accordion' | 'mappa') {
     this.vistaSelezione = vista;
     if (vista === 'mappa') {
-      setTimeout(() => this.inizializzaOAggiornaMappa(), 50);
+       requestAnimationFrame(() => this.inizializzaOAggiornaMappa());
     }
   }
 
   private inizializzaOAggiornaMappa() {
     if (!this.mapContainerRef) return;
 
-    if (!this.mappa) {
-      const defaultIcon = L.icon({
-        iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
-        iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
-        shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
-        iconSize: [25, 41],         
-        iconAnchor: [12, 41],       
-        popupAnchor: [1, -34],      
-        shadowSize: [41, 41]
-      });
-      L.Marker.prototype.options.icon = defaultIcon;
-      this.mappa = L.map(this.mapContainerRef.nativeElement).setView([38.1157, 13.3615], 13);
+    const defaultIcon = L.icon({
+      iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+      iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+      shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+      iconSize: [25, 41],         
+      iconAnchor: [12, 41],       
+      popupAnchor: [1, -34],      
+      shadowSize: [41, 41]
+    });
+    L.Marker.prototype.options.icon = defaultIcon;
+    this.mappa = L.map(this.mapContainerRef.nativeElement).setView([38.1157, 13.3615], 13);
 
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; OpenStreetMap contributors'
-      }).addTo(this.mappa);
-    }
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(this.mappa);
+    
 
     this.disegnaMarkerCopisterie();
     setTimeout(() => this.mappa?.invalidateSize(), 0);
